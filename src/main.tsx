84cbee02fabb6f18c7b859815c27bc799c47d425
BernydotJar <fearlesed@gmail.com>
2026-07-11 23:00:51 -0600

    Mount Teacher Intelligence workspace

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import StudentTwinWorkspace from './components/student-twin/StudentTwinWorkspace'
+import TeacherIntelligenceWorkspace from './components/teacher-intelligence/TeacherIntelligenceWorkspace'
 import './styles.css'
 import './student-twin.css'
+import './teacher-intelligence.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <StudentTwinWorkspace />
+    <TeacherIntelligenceWorkspace />
   </StrictMode>,
 )
