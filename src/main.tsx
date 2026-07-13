8a7ca6606973bcdb37e270e0adf5e5904b3a6e32
BernydotJar <fearlesed@gmail.com>
2026-07-13 08:37:08 -0600

    Make Learning Intelligence shell the default experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
-import StudentTwinWorkspace from './components/student-twin/StudentTwinWorkspace'
-import TeacherIntelligenceNavigationBridge from './components/teacher-intelligence/TeacherIntelligenceNavigationBridge'
-import TeacherIntelligenceWorkspace from './components/teacher-intelligence/TeacherIntelligenceWorkspace'
+import LearningIntelligenceApp from './app/LearningIntelligenceApp'
 import './styles.css'
 import './student-twin.css'
 import './teacher-intelligence.css'
+import './application-shell.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <StudentTwinWorkspace />
-    <TeacherIntelligenceWorkspace />
-    <TeacherIntelligenceNavigationBridge />
+    <LearningIntelligenceApp />
   </StrictMode>,
 )
